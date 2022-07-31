ccb8b08b3624f597a40b5560e25b312811444245
Samuel Almeida <88410126+SamuelWSGIT@users.noreply.github.com>
2022-07-31 00:12:46 -0300

    svg animados

--- style/main.js
+++ style/main.js
@@ -1,6 +1,7 @@
 const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
+
 function animeScroll() {
     const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
     target.forEach(function(element) {
