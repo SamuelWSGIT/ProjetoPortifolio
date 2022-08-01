62a9c1945cccaa56de22b6b86f810f128456afd8
SamuelWSGIT <88410126+SamuelWSGIT@users.noreply.github.com>
2022-08-01 17:57:25 -0300

    otimização

--- style/main.js
+++ style/main.js
@@ -1,11 +1,26 @@
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+        const context = this;
+        const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
 const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
-
 function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    const windowin = window.pageYOffset + ((window.innerHeight * 2.5) / 4);
+    const windowout = window.pageYOffset + ((window.innerHeight * 2.5) / 4);
     target.forEach(function(element) {
-        if((windowTop) > element.offsetTop) {
+        if((windowin) > element.offsetTop) {
             element.classList.add(animationClass);
         } else {
             element.classList.remove(animationClass);
@@ -17,4 +32,11 @@ animeScroll();
 
 window.addEventListener('scroll', function() {
     animeScroll();
+
+    if(target.length) {
+        window.addEventListener('scroll', debounce(function() {
+            animeScroll();
+            console.log('1');
+        }, 900));
+    }
 })
